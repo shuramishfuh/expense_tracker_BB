d268bf9ec2b48924bfef06fb8950eb4679f7e9ff
Ramish <agamatimtim@gmail.com>
2022-06-21 02:27:48 -0500

    updating ExpenseForm state with dependency on previous state not safe method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
     const expenses = [
@@ -24,6 +25,7 @@ function App() {
 
     return (
         <div>
+            <NewExpense/>
             <Expenses items={expenses}/>
         </div>
 
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,56 @@
+import './ExpenseForm.css'
+import {useState} from "react";
+
+//  manage state with one use state variable
+// ensuring to copy other variable that do not change
+//  back into the state variable
+const ExpenseForm = () => {
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredDate: '',
+        enteredAmount: '',
+    })
+
+    const titleChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        });
+    };
+    const amountChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value
+        });
+
+    };
+    const dateChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        });
+    };
+
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label> Title</label>
+                    <input type="text" onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label> Amount</label>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label> Date</label>
+                    <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add expense</button>
+            </div>
+        </form>
+    );
+};
+export default ExpenseForm;
