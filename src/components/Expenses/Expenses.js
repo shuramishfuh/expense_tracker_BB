abf64d10ff5672f6c5a6298536e039b1d7b9ba50
Ramish <agamatimtim@gmail.com>
2022-06-22 01:10:53 -0500

    lifting state up communication between ExpensesFilter and expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,28 +1,38 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from 'react'
 
 const Expenses = (props) => {
+    const [dateSelected, setDateSelected] = useState('');
+    const onselectDate = (dateSelected) => {
+        setDateSelected(dateSelected);
+        console.log(dateSelected);
+    }
 
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                date={props.items[0].date}
-                amount={props.items[0].Amount}
-                title={props.items[0].title}
-            />
-            <ExpenseItem
-                date={props.items[1].date}
-                amount={props.items[1].Amount}
-                title={props.items[1].title}
-            />
-            <ExpenseItem
-                date={props.items[2].date}
-                amount={props.items[2].Amount}
-                title={props.items[2].title}
-            />
+        <div>
+            <ExpensesFilter onselect ={onselectDate}/>
+            <Card className="expenses">
+                <ExpenseItem
+                    date={props.items[0].date}
+                    amount={props.items[0].Amount}
+                    title={props.items[0].title}
+                />
+                <ExpenseItem
+                    date={props.items[1].date}
+                    amount={props.items[1].Amount}
+                    title={props.items[1].title}
+                />
+                <ExpenseItem
+                    date={props.items[2].date}
+                    amount={props.items[2].Amount}
+                    title={props.items[2].title}
+                />
 
-        </Card>
+            </Card>
+        </div>
     )
 }
 
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,22 @@
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+    const OnselectChangeHandler = event => {
+        props.onselect(event.target.value);
+    }
+    return (
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+                <label>Filter by year</label>
+                <select onChange={OnselectChangeHandler}>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default ExpensesFilter;
