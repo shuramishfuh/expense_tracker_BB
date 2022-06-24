0edd2ea35426c6f695a4b18b3498a2be8058e917
Ramish <agamatimtim@gmail.com>
2022-06-24 02:50:06 -0500

    refactored expenses to ExpensesList (Item not found handle)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from 'react'
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const [filteredDate, setFilteredDate] = useState('2020');
@@ -14,26 +14,11 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredDate
     });
 
-    let items = <p> No Expenses found for this Year </p>;
-
-    if (filteredExpenses.length > 0) {
-        items = filteredExpenses.map(expense => <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}/>
-        )
-    }
-
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selectedYear={filteredDate} onselect={onselectDateHandler}/>
-                {
-                    items
-                };
-
-
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     )
